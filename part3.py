4d631800240a5b0ea990f5b0f106f53617180ba6
Manas Chan <94186677+manashunter@users.noreply.github.com>
2021-12-04 17:08:06 +0000

    I fixed the code and followed the questions to write the code.

--- part3.py
+++ part3.py
@@ -14,11 +14,12 @@ Enter a number or enter 0 to stop: 0
 Average: 6.0
 
 '''
-num = int(input("Enter a number or enter 0 to stop:"))
+num = int(input("Enter a number or enter 0 to stop: "))
 total = 0
-
+total_numbers = 0
 while num != 0:
   total = total + num
-  num = int(input("Enter a number or enter 0 to stop:"))
-    
-print("Average:", total)
+  total_numbers += 1
+  num = int(input("Enter a number or enter 0 to stop: "))
+  
+print("Average:", total / total_numbers)
--- part5.py
+++ part5.py
@@ -18,3 +18,12 @@ Enter a number or enter 0 to stop: 0
 Number of evens: 3
 
 '''
+even_numbers = 0
+number = int(input('Enter a number or enter 0 to stop: '))
+
+while number != 0:
+  if number %  2 == 0:
+    even_numbers += 1
+  number = int(input('Enter a number or enter 0 to stop: '))
+
+print('Number of evens: ', even_numbers)
